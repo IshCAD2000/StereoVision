01442db3a3e5eed8df5ac7badd8d7753726c402e
IshCAD2000 <81837576+IshCAD2000@users.noreply.github.com>
2022-09-17 12:05:08 +1000

    testinggithib desktop
    lmao

--- stereovision/blockmatchers.py
+++ stereovision/blockmatchers.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with StereoVision.  If not, see <http://www.gnu.org/licenses/>.
 
+
 '''
 Wrapper classes for block matching algorithms.
 
